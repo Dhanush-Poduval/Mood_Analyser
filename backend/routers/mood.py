623a97abdb87b564cacea9b74eff78ed71e98d95
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-09-09 01:14:58 +0530

    feat:made the get endpoints for all and specific

--- backend/routers/mood.py
+++ backend/routers/mood.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,APIRouter,Depends,Form,HTTPException,status
+from typing import List
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models,schemas
@@ -42,4 +43,18 @@ def delete_mood(mood_id:int,db:Session=Depends(get_db)):
       db.delete(moods)
       db.commit()
       return{'Delete':f'deleted the {moods.id} Mood '}
+
+@router.get('/moods',response_model=List[schemas.Showcase_Moods])
+def get_moods(user_id:int,db:Session=Depends(get_db)):
+      moods=db.query(models.Mood).filter(models.Mood.userid==user_id).first()
+      if not moods:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User not found")
+      return moods
+@router.get('/models/{id}',response_model=schemas.Showcase_Moods)
+def specific_mood(mood_id:int, db:Session=Depends(get_db)):
+      mood=db.query(models.Mood).filter(models.Mood.id==mood_id).first()
+      if not mood:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='No mood created')
+      return mood
+      
       
--- backend/schemas.py
+++ backend/schemas.py
@@ -15,9 +15,21 @@ class Login(BaseModel):
     email:str
     password:str
 
+class UserBase(BaseModel):
+    name: str
+    email: str
 
+    class Config:
+        orm_mode = True
 class Mood(BaseModel):
     mood:str
     content:str
     userid:int
-    
+
+class Showcase_Moods(BaseModel):
+    mood_set:str
+    content:str
+    userid:int
+    owner:UserBase
+    class Config():
+        orm_mode = True
