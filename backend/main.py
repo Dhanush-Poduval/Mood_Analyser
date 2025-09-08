4e847a05cca538ad4b3bd1fca9949ae6a4cbe017
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-09-08 21:33:21 +0530

    made the schemas and made the signup endpoint without jwt and hash

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
-from .database import engine
-from . import models
+from fastapi import FastAPI,Depends
+from .database import engine,get_db
+from . import models , schemas
+from sqlalchemy.orm import Session
 
 app=FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-@app.get('/test')
-def test():
-    return{"test":"working well"}
+@app.post('/signup')
+def create_user(signup:schemas.Show_user , db:Session=Depends(get_db)):
+    new_user=models.User(name=signup.name,email=signup.email,password=signup.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
 
+    return new_user
+
+    
