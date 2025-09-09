668e6f7df4ed57c4ac0ae2247e192569bab322ff
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-09-09 18:44:31 +0530

    feat:made the authentication for all endpoints

--- backend/routers/mood.py
+++ backend/routers/mood.py
@@ -7,11 +7,6 @@ from .. import models,schemas,oauth2
 app =FastAPI()
 
 router=APIRouter(tags=['Mood'])
-
-@router.post('/test')
-def test():
-    return 'Success'
-
 @router.post('/add_mood')
 def add_mood(moodshi:schemas.Mood,db:Session=Depends(get_db),current_user:schemas.Show_user=Depends(oauth2.get_current_user)):
         mood=models.Mood(mood_set=moodshi.mood,content=moodshi.content,userid=current_user.id)
@@ -27,7 +22,7 @@ def add_mood(moodshi:schemas.Mood,db:Session=Depends(get_db),current_user:schema
 
 @router.put('/update_mood')
 def update_mood(mood_id:int, updated_mood:schemas.Mood, db:Session=Depends(get_db),current_user:schemas.Show_user=Depends(oauth2.get_current_user)):
-    mood=db.query(models.Mood).filter(models.Mood.id==mood_id).first()
+    mood=db.query(models.Mood).filter(models.Mood.id==mood_id,models.Mood.userid==current_user.id).first()
     if not mood:
           raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Mood not found")
     mood.mood_set=updated_mood.mood
@@ -36,8 +31,8 @@ def update_mood(mood_id:int, updated_mood:schemas.Mood, db:Session=Depends(get_d
     return mood
 
 @router.delete('/delete')
-def delete_mood(mood_id:int,db:Session=Depends(get_db)):
-      moods=db.query(models.Mood).filter(models.Mood.id==mood_id).first()
+def delete_mood(mood_id:int,db:Session=Depends(get_db),current_user:schemas.Show_user=Depends(oauth2.get_current_user)):
+      moods=db.query(models.Mood).filter(models.Mood.id==mood_id,models.Mood.userid==current_user.id).first()
       if not moods:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="No mood log found")
       db.delete(moods)
@@ -45,14 +40,14 @@ def delete_mood(mood_id:int,db:Session=Depends(get_db)):
       return{'Delete':f'deleted the {moods.id} Mood '}
 
 @router.get('/moods',response_model=List[schemas.Showcase_Moods])
-def get_moods(user_id:int,db:Session=Depends(get_db)):
-      moods=db.query(models.Mood).filter(models.Mood.userid==user_id).first()
+def get_moods(db:Session=Depends(get_db),current_user:schemas.Show_user=Depends(oauth2.get_current_user)):
+      moods=db.query(models.Mood).filter(models.Mood.userid==current_user.id).all()
       if not moods:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="User not found")
       return moods
 @router.get('/models/{id}',response_model=schemas.Showcase_Moods)
-def specific_mood(mood_id:int, db:Session=Depends(get_db)):
-      mood=db.query(models.Mood).filter(models.Mood.id==mood_id).first()
+def specific_mood(mood_id:int, db:Session=Depends(get_db),current_user:schemas.Show_user=Depends(oauth2.get_current_user)):
+      mood=db.query(models.Mood).filter(models.Mood.id==mood_id,models.Mood.userid==current_user.id).first()
       if not mood:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='No mood created')
       return mood
