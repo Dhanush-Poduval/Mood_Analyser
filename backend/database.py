16bfe048fc700b977a7236b313499c496009dfb5
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-09-09 23:33:33 +0530

    feat:made the url to databse in env

--- backend/database.py
+++ backend/database.py
@@ -4,9 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 DATABASE_URL=os.getenv('DATABASE_URL',"sqlite:///./mood.db")
-
-
-
 engine=create_engine(DATABASE_URL,connect_args={"check_same_thread":False} if DATABASE_URL.startswith("sqlite")else{})
 SessionLocal=sessionmaker(bind=engine,autoflush=False,autocommit=False)
 Base=declarative_base()
