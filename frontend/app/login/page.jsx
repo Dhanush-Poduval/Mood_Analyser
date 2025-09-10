654b60d0e206b8f4591501f6f400ed1a249aaf0e
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-09-10 11:24:34 +0530

    feat:made the login page design and multiple pages

---
+++ frontend/app/login/page.jsx
@@ -0,0 +1,8 @@
+import Login from '@/components/Login'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex justify-center mt-32'><Login /></div>
+  )
+}
